9820ce864fbf147c007cd9af9380039cc20baaf3
Bektur13 <duyshembekov04@mail.ru>
2025-04-10 12:21:12 -0700

    Added files

---
+++ ContactManagementSystem/src/main/java/com/contactManagement/ContactManagement.java
@@ -0,0 +1,5 @@
+package com.contactManagement;
+
+public class ContactManagement {
+
+}
---
+++ SchoolManagement/src/test/java/school/management/CourseManagerTest.java
@@ -0,0 +1,9 @@
+package school.management;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseManagerTest {
+    
+}
